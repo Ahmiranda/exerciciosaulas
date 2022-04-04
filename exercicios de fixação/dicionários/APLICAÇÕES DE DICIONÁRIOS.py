b0ce5106e5327b1ce454452965bab55806132f73
Alan Miranda <51090438+ahpqmiranda@users.noreply.github.com>
2022-04-04 14:09:36 -0300

    commit noite de 22/03/2022

---
+++ exercicios de fixação/dicionários/APLICAÇÕES DE DICIONÁRIOS.py
@@ -0,0 +1,11 @@
+# Dicionários - mapas (em outras linguagens)
+# dicionários são coleções do tipo chave-valor
+
+print(type({}))  # retorna a classe dict
+
+paises = {'br': "Brasil", 'US': "Estados unidos", 'JP': "Japão", 'UK': "Reino Unido"}
+print(paises)
+print(type(paises))
+# os dicionários possuem estrutra 'chave':'valor'
+# não há limitações sobre as categorias de dados empregados
+# podemos misturar tipos de dados
