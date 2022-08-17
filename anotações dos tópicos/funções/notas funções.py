ec995d50e2f3613575c6407294629c6a15c4295e
Alan <alan.adm2011@gmail.com>
2022-08-16 23:55:59 -0300

    anotações de funções

---
+++ anotações dos tópicos/funções/notas funções.py
@@ -0,0 +1,20 @@
+"""
+DEFININDO FUNÇÕES
+
+Funções são pequenas partes de códigos que realizam tarefas específicas.
+Funções podem ou não receber entradas de dados e retornar uma saída de dados.
+São úteis para realizar procedimentos repetitivos e para agrupar códigos que podem ser reutilizados.
+
+
+"""
+
+# exemplos
+cores = ['verde', 'amarelo', 'azul', 'branco']
+# funções integradas em python: Built in functions
+print(cores)
+
+# funções criadas pelo usuário: Custom functions
+
+
+def cores_escolhidas(lista_cores):
+    print(lista_cores)
